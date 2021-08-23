754d7e7e5ad8e8b3dd3caad3241e63abd6984c68
Cristian Magana <cmagana@secureauth.com>
2021-08-22 19:02:18 -0700

    datatypes, and some syntax

---
+++ 02_packages/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"math"
+
+	"github.com/digitalxtian/golang/02_packages/strutil"
+)
+
+func main() {
+	fmt.Println(math.Floor(3.7))
+	fmt.Println(strutil.Reverse("hello"))
+}
