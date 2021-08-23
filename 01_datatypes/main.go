ac44192b09ead097fc982fe6a3c8313929fd28e5
Cristian Magana <cmagana@secureauth.com>
2021-08-22 19:04:43 -0700

    datatypes, and some syntax

--- 01_datatypes/main.go
+++ 01_datatypes/main.go
@@ -15,11 +15,11 @@ func main() {
 	// float32 float64
 	// complex64 complex128
 
-
 	// Datatypes are infered upon declaration. 
 	name := "Cristian"
 	age  := 20
 	isMale := 1
+	fmt.Println(name, age, isMale)
 
 	// array
 	arrayEx := [5]string{"array","of","fixed","size","0-based"}
@@ -29,6 +29,25 @@ func main() {
 	sliceEx := []string{"c++","like","vector"}
 	fmt.Println(sliceEx)
 
-	fmt.Println(name, age, isMale)
+	// conditionals
+	color := "red"
 
+	if color == "red" {
+		fmt.Println("is red")
+	} else if color == "blue" {
+		fmt.Println("is blue")
+	} else {
+		fmt.Println("not blue nor red")
+	}
+
+	// switch 
+	switch color {
+	case "red":
+		fmt.Println("is red")
+	case "blue":
+		fmt.Println("is blue")
+	default:
+		fmt.Println("not blue nor red")
+
+	}
 }
